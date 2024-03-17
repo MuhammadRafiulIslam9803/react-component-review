d23939efe3988461533d89e410b4eeb0d5ac11fd
Rafiul Islam <www.rafiulislam1998@gmail.com>
2024-03-17 20:37:56 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const array = [
+    {name :'Rafiul' ,age :23 , gender : 'Male'},
+    {name :'Sayma' ,age :15 , gender : 'Female'},
+    {name :'Noyon' ,age :14 , gender : 'Female'},
+    {name :'Liza' ,age :14 , gender : 'Female'},
+    {name :'Ajmira' ,age :22 , gender : 'Female'},
+    {name :'Mohona' ,age :15 , gender : 'Female'},
+    {name :'Forget' ,age :14 , gender : 'Female'}
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        array.map(person => <Component name={person.name} age={person.age} gender={person.gender}></Component>)
+      }
     </div>
   );
+}
+function Component (props){
+  return (
+    <div className='style'>
+      <h1>Name : {props.name} </h1>
+      <h3>Age : {props.age} </h3>
+      <h4>Gender : {props.gender}</h4>
+    </div>
+  )
 }
 
 export default App;
